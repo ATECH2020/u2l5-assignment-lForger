69a05274347307571d1f7b0ca58e072e53debdc3
Bulmaro Nava-Camal <bulmaro.201312@nv.ccsd.net>
2020-09-28 00:13:59 +0000

    Copied and pasted how far away from an earlier replit

--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -1,9 +1,32 @@
 import java.util.Scanner;
-
-public class HowFarAway 
+ 
+public class HowFarAway
 {
-    public static void main(String[] args)
-    {
-        // your code here.
-    }
+   public static void main(String[] args)
+   {
+       //Creates a scanner
+       Scanner in = new Scanner(System.in);
+       //Creates all varriables
+       double firstLatitude, firstLongitude, otherLatitude, otherLongitude, distance = 0;
+       //Accepts longitude and latitude of both locations from user
+       System.out.println("Input the first latitude ");
+       firstLatitude = in.nextDouble();
+       System.out.println("Input the first longitude ");
+       firstLongitude = in.nextDouble();
+       System.out.println("Input the second latitude ");
+       otherLatitude = in.nextDouble();
+       System.out.println("Input the second longitude ");
+       otherLongitude = in.nextDouble();
+       //Creates two geoLocation objects
+       GeoLocation firstLocation = new GeoLocation(firstLatitude, firstLongitude);
+ 
+       GeoLocation otherLocation = new GeoLocation(otherLatitude, otherLongitude);
+       //Finds and prints the distance between the two locations
+       distance = firstLocation.distanceFrom(otherLocation);
+ 
+       System.out.println(distance);
+ 
+    
+     
+   }
 }
